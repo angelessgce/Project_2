873d0555eeaf58a0fe5605c25f84385f0a7e5e12
anedelasfuentes <150723233+anedelasfuentes@users.noreply.github.com>
2023-11-20 13:14:17 -0500

    Update GameEngine.py
    Version 2 of GameEngine

--- GameEngine.py
+++ GameEngine.py
@@ -5,8 +5,10 @@
 # Description: this program will contain the function necessary to initialize all the data for the game
 
 import os  # To check if the file exists
+import random
 
 from Veggie import Veggie
+
 
 class GameEngine:
     NUMBEROFVEGGIES = 30
@@ -37,13 +39,31 @@ class GameEngine:
             if lineNumber == 1:  # the first line is the size of the grid
                 data = line.strip().split(',')
                 height = int(data[-2])  # clean the data
-                width = int(data[-1])   # clean the data
+                width = int(data[-1])  # clean the data
 
             else:
-                # This needs to be an object
-
                 data = line.strip().split(',')  # the subsequent lines are added to the list of possible vegetables
                 veggies = Veggie(data[0], data[1], data[2])
                 self.__vegetables.append(veggies)  # I add it to the updated list of possible vegetables
 
+        for i in range(height):  # We create an empty 2D list for the field
+            row = []
+            for j in range(width):
+                row.append(None)
+            self.__field.append(row)
+
+        for vegetable in range(GameEngine.NUMBEROFVEGGIES):  # We iterate through that empty 2D list
+            chosenVeggie = random.randrange(len(self.__vegetables))  # We get a random veggie
+            col = random.randrange(width)  # We generate a random position
+            row = random.randrange(height)
+
+            while self.__field[row][col] != None:  # While that position is not empty
+                col = random.randrange(width)  # We generate a random position
+                row = random.randrange(height)
+
+            self.__field[row][col] = self.__vegetables[chosenVeggie].getInhabitSymbol()
+
         myFile.close()
+
+
+
