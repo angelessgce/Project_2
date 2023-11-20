b6cc21f793052cd12eaaa66f337ce9c56747501d
anedelasfuentes <150723233+anedelasfuentes@users.noreply.github.com>
2023-11-20 18:13:47 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ from GameEngine import GameEngine
 
 def main():
     game = GameEngine()
-    game.initVeggies()
-    game.moveRabbits()
-
     game.initializeGame()
     game.intro()
     print(game.remainingVeggies())
+    print("Before moving rabbits:")
     game.printField()
-
+    game.moveRabbits()
+    print("After moving rabbits:")
+    game.printField()
 
 main()
