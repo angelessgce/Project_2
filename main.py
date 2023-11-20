99909e7c4631181a5733aad807a8ef0e0932bf41
anedelasfuentes <150723233+anedelasfuentes@users.noreply.github.com>
2023-11-20 13:16:11 -0500

    Upload main

---
+++ main.py
@@ -0,0 +1,14 @@
+# Author: de las Fuentes Monreal,    Ane
+#         Gonzalez Castro,          Maria Angeles
+#         Gonzalez Rodriguez,       Daniel
+# Date: November 20th 2023
+# Description: this program will contain all the necessary function calls
+
+from GameEngine import GameEngine
+
+def main():
+    game = GameEngine()
+    game.initVeggies()
+
+
+main()
