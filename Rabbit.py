0819a903351d0b9f86076eeccdbfe677ece239d1
anedelasfuentes <150723233+anedelasfuentes@users.noreply.github.com>
2023-11-30 15:59:12 -0500

    Files upload with finished functions in some
    Finished function in Rabbit, Veggie, Creature, Captain, and FieldInhabitant. Need to finish main and GameEngine.

--- Rabbit.py
+++ Rabbit.py
@@ -8,6 +8,14 @@
 from Creature import Creature
 
 class Rabbit(Creature):
-    def __init__(self,x,y):
-        Creature.__init__(self, x, y, "R")
-
+    def __init__(self, x, y):
+        """
+        This function defines a constructor to initialize class variables of Rabbit and objects or setup class data
+        structures. It takes in the coordinates x and y, and returns nothing
+        :param x: determines the position x
+        :type x: int
+        :param y: determines the position y
+        :type y: int
+        """
+        Creature.__init__(self, x, y, "R")  # We initialize said coordinates and associate the symbol "R" to
+        # the inhabitant rabbit
